a8bedfa8f68f28ae42581f8897242f50ca240216
Eashan <greyhatem@gmail.com>
2024-11-16 15:34:55 +0530

    working

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from pg import playground
 
 pg = playground([10, 10])
-pg.box([1,1], [5, 5])
+for i in range(1, 6):
+  pg.obs((1, i))
+  pg.obs((6, i))
+  pg.obs((i, 1))
+  pg.obs((i, 6))
+pg.obs((6, 6))
+pg.start((2, 2))
 pg.draw()
+while not pg.escaped():
+  msg = pg.move(1, pg.RIGHT)
+  if msg=="OBS_FOUND":
+    break
+  pg.draw()
--- pg.py
+++ pg.py
@@ -3,16 +3,15 @@ from termcolor import colored
 class playground():
   ABS = 0
   REL = 1
-  UP = 0
-  RIGHT = 1
-  DOWN = 2
-  LEFT = 3
+  UP = (-1, 0)
+  RIGHT = (0, 1)
+  DOWN = (1, 0)
+  LEFT = (-1, 0)
 
   pers = ABS
   area = []
   direction = RIGHT
-  turtlePos = []
-  boxOri = []
+  turtlePos = (0, 0)
   boxArea = []
   def __init__(self, pos):
     self.area = pos
@@ -20,16 +19,27 @@ class playground():
     turtlePos = pos
   def perspective(self, pers):
     self.pers = pers
-  def box(self, ori, pos):
-    self.boxOri = ori
-    self.boxArea = pos
-  def move(self):
-    return 0
+  def obs(self, pos):
+    self.boxArea.append(pos)
+  def start(self, pos):
+    self.turtlePos = pos
+  def move(self, dis, angle):
+    temp = (self.turtlePos[0]+angle[0]*dis, self.turtlePos[1]+angle[1]*dis)
+    if not (temp in self.boxArea):
+      self.turtlePos = temp
+    else:
+      return "OBS_FOUND"
+  def escaped(self):
+    if (self.turtlePos[0]<1 or self.turtlePos[0]>=6) or (self.turtlePos[1]<1 or self.turtlePos[1]>=6):
+      return True
+    return False
   def draw(self):
     for i in range(0, self.area[0]):
       for j in range(0, self.area[1]):
-        if (j>=self.boxOri[0] and j<self.boxOri[0]+self.boxArea[0]) and (i==self.boxOri[1] or i==self.boxOri[1]+self.boxArea[1]):
+        if (i, j) in self.boxArea:
           print(colored('= ', 'yellow'), end='')
+        elif (i, j)==self.turtlePos:
+          print(colored('@ ', 'green'), end='')
         else:
           print('# ', end="")
       print()
