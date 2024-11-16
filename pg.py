30e67230a3370e9e31544c1c8e43e1abccd4dba6
Eashan-EM <86050560+Eashan-EM@users.noreply.github.com>
2024-11-16 17:33:11 +0530

    Update pg.py

--- pg.py
+++ pg.py
@@ -6,40 +6,47 @@ class playground():
   UP = (-1, 0)
   RIGHT = (0, 1)
   DOWN = (1, 0)
-  LEFT = (-1, 0)
+  LEFT = (0, -1)
 
   pers = ABS
   area = []
-  direction = RIGHT
   turtlePos = (0, 0)
-  boxArea = []
   def __init__(self, pos):
-    self.area = pos
-  def turtle_pos(self, pos):
-    turtlePos = pos
+    for i in range(0, pos[0]):
+      self.area.append([])
+      for j in range(0, pos[1]):
+        self.area[i].append(0)
+  def setPos(self, pos):
+    self.turtlePos = pos
+    self.area[pos[0]][pos[1]] = 3
+  def getPos(self):
+    return self.turtlePos
   def perspective(self, pers):
     self.pers = pers
   def obs(self, pos):
-    self.boxArea.append(pos)
-  def start(self, pos):
-    self.turtlePos = pos
-  def move(self, dis, angle):
-    temp = (self.turtlePos[0]+angle[0]*dis, self.turtlePos[1]+angle[1]*dis)
-    if not (temp in self.boxArea):
+    self.area[pos[0]][pos[1]] = 1
+  def setEnd(self, pos):
+    self.area[pos[0]][pos[1]] = 2
+  def move(self, angle):
+    pos = self.turtlePos
+    temp = (pos[0]+angle[0], pos[1]+angle[1])
+    if not (self.area[temp[0]][temp[1]]==1 or self.area[temp[0]][temp[1]]==2):
+      self.area[pos[0]][pos[1]] = 4
       self.turtlePos = temp
+      self.area[temp[0]][temp[1]] = 3
     else:
       return "OBS_FOUND"
-  def escaped(self):
-    if (self.turtlePos[0]<1 or self.turtlePos[0]>=6) or (self.turtlePos[1]<1 or self.turtlePos[1]>=6):
-      return True
-    return False
   def draw(self):
-    for i in range(0, self.area[0]):
-      for j in range(0, self.area[1]):
-        if (i, j) in self.boxArea:
+    for i in range(0, len(self.area)):
+      for j in range(0, len(self.area[0])):
+        if self.area[i][j] == 0:
+          print('# ', end='')
+        elif self.area[i][j] == 1:
           print(colored('= ', 'yellow'), end='')
-        elif (i, j)==self.turtlePos:
+        elif self.area[i][j] == 2:
+          print(colored('# ', 'red'), end='')
+        elif self.area[i][j] == 3:
           print(colored('@ ', 'green'), end='')
-        else:
-          print('# ', end="")
+        elif self.area[i][j] == 4:
+          print(colored('# ', 'green'), end='')
       print()
